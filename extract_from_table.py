f9615f5f63ee77212e8ff72b50ba90640c3733ff
brianShan974 <2366910832@qq.com>
2024-03-10 08:08:27 +0000

    Now extracts the data correctly.

--- extract_from_table.py
+++ extract_from_table.py
@@ -2,7 +2,7 @@
 file_name = "./fort.1900.N2O.456.20211108"
 
 # new_file_name = "extracted.dat"
-new_file_name = "extracted_j_l.dat"
+new_file_name = "extracted_j_l_sorted_by_energy.dat"
 
 
 def is_valid(line: str) -> bool:
@@ -10,8 +10,44 @@ def is_valid(line: str) -> bool:
     return splitted_line[0] == splitted_line[6]
 
 
-with open(file_name) as f:
-    lines = [line for line in f.readlines() if is_valid(line)]
+def get_p_from_line(line: str) -> str:
+    splitted_line = line.split()
+    v1, v2, v3 = [int(splitted_line[i]) for i in (4, 5, 7)]
+    return str(2 * v1 + v2 + 4 * v3)
 
-with open(new_file_name, 'w') as f:
-    f.writelines(lines)
+
+def get_l_from_line(line: str) -> str:
+    return line.split()[6]
+
+
+def line_key(line: str) -> str:
+    return get_p_from_line(line) + " " + get_l_from_line(line)
+
+
+def sort_key(line: str) -> float:
+    return float(line.split()[3])
+
+
+def main():
+    with open(file_name) as f:
+        lines = [line for line in f.readlines() if is_valid(line)]
+
+    table: dict[str, list[str]] = {}
+
+    for line in lines:
+        key = line_key(line)
+        if key not in table:
+            table[key] = [line]
+        else:
+            table[key].append(line)
+
+    for key in table.keys():
+        table[key].sort(key=sort_key)
+
+    with open(new_file_name, "w") as f:
+        for value in table.values():
+            f.writelines(value)
+
+
+if __name__ == "__main__":
+    main()
--- get_n.py
+++ get_n.py
@@ -48,7 +48,7 @@
 # file_name = "n2o.12iso.296K.1E-31.15Kcm-1.Y02-A8.dmsC.v2.dat"
 
 # format j e/f # E v1 v2 l v3 useless u
-file_name = "./extracted_j_l.dat"
+from extract_from_table import new_file_name as file_name
 
 # label = "42"
 
@@ -61,36 +61,5 @@ file_name = "./extracted_j_l.dat"
 # with open(new_file_name, 'w') as f:
 #     f.writelines(lines)
 
-def get_p_from_line(line: str) -> str:
-    splitted_line = line.split()
-    v1, v2, v3 = [int(splitted_line[i]) for i in (4, 5, 7)]
-    return str(2 * v1 + v2 + 4 * v3)
 
-
-def get_l_from_line(line: str) -> str:
-    return line.split()[6]
-
-
-def line_key(line: str) -> str:
-    return get_p_from_line(line) + " " + get_l_from_line(line)
-
-
-def sort_key(line:str) -> float:
-    return float(line.split()[3])
-
-
-n_table: dict[str, list[str]] = {}
-
-with open(file_name) as f:
-    lines = f.readlines()
-
-for line in lines:
-    key = line_key(line)
-    if key not in n_table:
-        n_table[key] = [line]
-    else:
-        n_table[key].append(line)
-
-for key in n_table.keys():
-    n_table[key].sort(key=sort_key)
-
+n_table = 
